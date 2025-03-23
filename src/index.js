1767f5977bd643d9ba87a531166e6b09a47237f0
Adrian <adrian.amoke@gmail.com>
2025-03-23 14:31:25 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
+
   
 }
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const message = formData.get('message');
+    console.log(name, email, message);
+}
+
 
 document.addEventListener('DOMContentLoaded', init);
